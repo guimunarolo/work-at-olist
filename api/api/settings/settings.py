9badddd7fdafc91a69d67f6eaac1706059199c30
Gui Munarolo <guimunarolo@hotmail.com>
2018-05-15 13:18:05 -0300

    Created and tested CallEventSerializer

--- api/api/settings/settings.py
+++ api/api/settings/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # libs
+    'rest_framework',
+
     # aplication
     'telephony.apps.TelephonyConfig',
 ]
