ba12c2cc1c78c37ad02ae6e4f1d69bea495cabf7
Gui Munarolo <guimunarolo@hotmail.com>
2018-05-21 12:51:17 -0300

    Finished bill report and fully tested.

--- api/telephony/services/reports.py
+++ api/telephony/services/reports.py
@@ -1,4 +1,5 @@
 from datetime import time, date, datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
 from django.db.models import OuterRef, Subquery
 
@@ -6,27 +7,37 @@ from telephony.models import CallEvent
 
 
 class BillReport:
+    """
+    Responsible to generate a Bill Reportself.
+
+    Args:
+        subscriber - Subscriber phone number
+        query_params (opitional) - filters month and year
+    """
+
     # fixed charges to pay the connection
     STANDING_CHARGE = 0.36
 
     # rules for standard tariff (between 6h00 and 22h00)
-    STANDARD_PRICE = 0.9 
-    STANDARD_START_TIME = time(6, 0, 0)
-    STANDARD_END_TIME = time(22, 0, 0)
+    STANDARD_CHARGE_PRICE = 0.09
+    STANDARD_CHARGE_START_TIME = time(6, 0, 0)
+    STANDARD_CHARGE_END_TIME = time(22, 0, 0)
 
     # rules for reduced tariff (between 22h00 and 6h00)
     REDUCED_PRICE = 0.0
 
     def __init__(self, subscriber, query_params={}, *arg, **kwargs):
+        """Set initial attributes."""
         self.subscriber = subscriber
-        self.cleaned_filters = self.clean_filters(query_params)
-    
-    def clean_filters(self, query_params):
-        '''
-        Returns month and year filters.
+        self.cleaned_filters = self._clean_filters(query_params)
+
+    def _clean_filters(self, query_params):
+        """
+        Return month and year filters.
+
         Month needs to be a closed month, in other words it will get the
         previous month or lesser.
-        '''
+        """
         today = date.today()
         default_month = today.month - 1
         default_year = today.year
@@ -57,15 +68,12 @@ class BillReport:
             year -= 1
 
         return {
-            'created__month': month,
-            'created__year': year,
+            'ended__month': month,
+            'ended__year': year,
         }
 
     def get_queryset(self):
-        '''
-        Returns a queryset with ended call events with start created field
-        annotated.
-        '''
+        """Return a queryset with completed calls."""
         end_query = CallEvent.ended.filter(call_id=OuterRef('call_id'))\
                                    .order_by('-created')\
                                    .values('created')[:1]
@@ -75,37 +83,90 @@ class BillReport:
                                             **self.cleaned_filters)
         return queryset
 
-    def format_obj(self, obj):
+    def _calc_duration(self, start, end):
+        """
+        Resturn a relativedelta between start/endself.
+
+        Args:
+            start - datetime
+            end - datetime
+        """
+        return relativedelta(end, start)
+
+    def _dates_between(self, start_date, end_date):
+        """
+        Generate dates between start and end.
+
+        Args:
+            start_date - date
+            end_date - date
+        """
+        start_date = datetime.combine(start_date, time.min)
+        end_date = datetime.combine(end_date, time.min)
+        days_between = (end_date - start_date).days
+        for x in range(0, days_between + 1):
+            yield start_date + timedelta(x)
+
+    def _calc_charge(self, call_start, call_end):
+        """
+        Implement the tarif logic.
+
+        Args:
+            call_start - datetime
+            call_end - datetime
+        """
+        charges = [self.STANDING_CHARGE]
+
+        for loop_date in self._dates_between(call_start.date(),
+                                             call_end.date()):
+            charge_start = datetime.combine(loop_date,
+                                            self.STANDARD_CHARGE_START_TIME)
+            charge_end = datetime.combine(loop_date,
+                                          self.STANDARD_CHARGE_END_TIME)
+
+            if call_start > charge_start:
+                charge_start = call_start
+
+            if call_end < charge_end:
+                charge_end = call_end
+
+            charge_minutes = int(
+                (charge_end-charge_start).total_seconds() / 60)
+            if charge_minutes > 0:
+                charges.append(charge_minutes * self.STANDARD_CHARGE_PRICE)
+
+        return sum(charges)
+
+    def _format_duration(self, duration):
+        hours = duration.days*24 + duration.hours
+        return '{}h{}m{}s'.format(hours, duration.minutes,
+                                  duration.seconds)
+
+    def _format_charge(self, charge):
+        return 'R$ {:.2f}'.format(charge).replace('.', ',')
+
+    def _format_obj(self, obj):
+        duration = self._calc_duration(obj.created, obj.ended)
+        charge = self._calc_charge(obj.created, obj.ended)
         return {
             'destination': obj.destination,
             'start_date': obj.created.date(),
             'start_time': obj.created.time(),
-            'duration': 0,
-            'price': 0.0
+            'duration': self._format_duration(duration),
+            'price': self._format_charge(charge),
+        }
+
+    def _format_filters(self, filters):
+        return {
+            'month': filters.get('ended__month'),
+            'year': filters.get('ended__year'),
         }
 
     def make_report(self):
+        """Return a dict with full report response."""
         queryset = self.get_queryset()
         return {
             'count': queryset.count(),
-            'filters': self.cleaned_filters,
-            'objects': [self.format_obj(obj) for obj in queryset.iterator()],
+            'filters': self._format_filters(self.cleaned_filters),
+            'objects': [self._format_obj(obj) for obj in queryset.iterator()],
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
