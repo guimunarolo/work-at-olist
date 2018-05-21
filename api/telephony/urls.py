54bed251e75e4e6fcfa3d9490defb819920efee3
Gui Munarolo <guimunarolo@hotmail.com>
2018-05-21 18:53:49 -0300

    Fixed telephony.urls identation and imports

--- api/telephony/urls.py
+++ api/telephony/urls.py
@@ -1,11 +1,10 @@
-from django.conf.urls import url, include
+from django.conf.urls import include, url
 
-from .resources import (
-	CallEventResource, BillReportResource)
+from .resources import BillReportResource, CallEventResource
 
 urlpatterns = [
     url(r'^call-events/$', CallEventResource.as_view(),
         name='telephony_call_events_resource'),
-    url(r'^bill-report/(?P<subscriber>[0-9]{11,12})/$', 
-    	BillReportResource.as_view(), name='telephony_bill_report_resource'),
+    url(r'^bill-report/(?P<subscriber>[0-9]{11,12})/$',
+        BillReportResource.as_view(), name='telephony_bill_report_resource'),
 ]
