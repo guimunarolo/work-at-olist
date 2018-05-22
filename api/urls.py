04a98884323323f386b87c286d19b0c36551d97b
Gui Munarolo <guimunarolo@hotmail.com>
2018-05-22 12:36:43 -0300

    Deployed 5d24ea0 with MkDocs version: 0.17.3

--- api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import include, url
-
-
-urlpatterns = [
-    url(r'^v1/telephony/', include('telephony.urls')),
-]
--- telephony/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class TelephonyConfig(AppConfig):
-    name = 'telephony'
